f406676682c0c59e3a4adebf3577533e3542c12f
maricelapotoc <maricelapotoc@gmail.com>
2022-11-24 00:48:48 +0200

    create blogRoutes file, add middlewares in app.js

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+// 3rd party modules
 import express from "express";
 import path from "path";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+// Local modules
+import blogRoutes from "./routes/blogRoutes";
+
+// Server initialization
 const app = express();
 const PORT = process.env.PORT;
 
+// Middlewares
+app.use(express.json()); // parse the incoming requests with JSON payloads
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", blogRoutes);
+
+// Server listen and connection to database
 mongoose.connect(`${process.env.MONGODB_URI}`);
 app.listen(PORT || "0.0.0.0", (error) => {
   if (!error) console.log("Server running on port" + PORT);
